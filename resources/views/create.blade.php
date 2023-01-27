aa2fec6f37b5ae297050c8f59b62564c58c64e97
Chanudi Tharushika <87629005+IT20029586@users.noreply.github.com>
2023-01-27 23:00:18 +0530

    Crud Completed

---
+++ resources/views/create.blade.php
@@ -0,0 +1,58 @@
+@extends('app')
+   
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2 style="color:#004d00;font-weight:bold;">Add New Product</h2>
+        </div>
+    </div>
+</div>
+      
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+ 
+<form action="{{ route('store') }}" method="POST" enctype="multipart/form-data">
+    @csrf
+     
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <input type="text" name="name" class="form-control" placeholder="Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description:</strong>
+                <textarea class="form-control" style="height:150px" name="description" placeholder="Description"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Price:</strong>
+                <input type="text" name="price" class="form-control" placeholder="Rs.5500.00">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Image:</strong>
+                <input type="file" name="image" class="form-control" placeholder="image">
+            </div>
+        </div>
+        <br>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center" style = "margin-top:20px">
+                <button style="background-color:#00873E" type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+      
+</form>
+@endsection
---
+++ resources/views/show.blade.php
@@ -0,0 +1,32 @@
+@extends('app')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2 style="color:#004d00;font-weight:bold;"> {{ $product->name }}</h2>
+            </div>
+        </div>
+    </div>
+      
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description:</strong>
+                {{ $product->description }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Price:</strong>
+                {{ $product->price }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Image:</strong>
+                <img src="/images/{{ $product->image }}" width="300px">
+            </div>
+        </div>
+    </div>
+@endsection
